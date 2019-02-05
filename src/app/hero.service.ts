02304f69dc6e902136de751889644c863f467709
Caleb Christensen <Caleb.Christ@gmail.com>
2019-02-05 18:03:28 -0500

    GET heroes by ID

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -25,6 +25,15 @@ export class HeroService {
     );
   }
 
+  //GET hero by id. Will 404 if id not found
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
   getHero(id: number): Observable<Hero> {
     //TODO: send the message _after+ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
